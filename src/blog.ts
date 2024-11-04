641c974fb7a2bc45d6ee5552c16ecb4d057c4eea
karinaleee <klee396@calpoly.edu>
2024-11-04 14:41:23 -0800

    hi

--- src/blog.ts
+++ src/blog.ts
@@ -21,3 +21,36 @@ const blog: Blog[] = [
 function appendBlogsToContainer(){
     const blogContainer = document.getElementById('blog-container');
 }
+blog.forEach(blog => {
+	const blogDiv = document.createElement("div");
+    blogDiv.className = "blog-post";
+
+
+    const blogTitle = document.createElement("h1");
+    blogTitle.textContent = blog.title; 
+    blogDiv.appendChild(blogTitle);
+
+        
+    const blogDate = document.createElement("p"); 
+    blogDate.textContent = blog.date; 
+    blogDiv.appendChild(blogDate);
+
+    
+    const blogDescription = document.createElement("p"); 
+    blogDescription.textContent = blog.date; 
+    blogDiv.appendChild(blogDescription);
+
+    const blogImage = document.createElement("img");
+    blogImage.src = blog.image; 
+    blogImage.width = 350;
+    blogImage.height = 250; 
+    blogImage.alt = "Image ${blog.title}"
+    blogDiv.appendChild(blogImage);
+    
+    const blogSlug = document.createElement("a"); 
+    blogSlug.href = blog.slug;
+    blogSlug.textContent = "Read this too"; 
+    blogDiv.appendChild(blogSlug);
+
+
+});
