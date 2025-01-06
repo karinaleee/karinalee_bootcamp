324dcc8903631219e6b0ba46f38d9f9afe677c3d
karinaleee <klee396@calpoly.edu>
2025-01-06 13:39:53 -0800

    comment

--- bootcamp-milestone-2/src/components/blogPreview/newcomment.tsx
+++ bootcamp-milestone-2/src/components/blogPreview/newcomment.tsx
@@ -1,92 +1,77 @@
 "use client";
-import React, { useState} from "react";
+import React, { useState } from "react";
 import style from "./newcomment.module.css";
 
-//type definitions
 type commentData = {
   user: string;
   content: string;
 };
 
 type Props = {
-  slug: string; 
+  slug: string;
 };
 
-
 export default function NewComment({ slug }: Props) {
-  //   const [error, setError] = useState("");
-  // creting constant for the new comment data and the status of the form input
-  //console.log("entering the new comment function")
   const [formData, setformData] = useState<commentData>({
     user: "",
     content: "",
   });
 
-
-  
   const [status, setStatus] = useState("");
 
-
-  console.log('status  1  ',status)
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
-    setformData((form) => ({ ...form, [name]: value }))
+    setformData((form) => ({ ...form, [name]: value }));
   };
-  
- 
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("in handle submit")
-    
-    console.log("entering try")
-   
+
     try {
-   
-      
-      const response = await fetch(`https://karinalee-bootcamp-97e9.vercel.app/api/Blogs/${slug}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        `https://karinalee-bootcamp-97e9.vercel.app/api/Blogs/${slug}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
-      console.log('response', response)
-
-      console.log('FORM DATA                   ', formData)
       if (!response.ok) {
         throw new Error("Failed to add comment.");
       }
 
+      // Update status and reset form data
       setStatus("submitted");
       setformData({ user: "", content: "" });
-      console.log('status  2  ',status)
-
-
     } catch (error) {
-      console.log("error", error);
       setStatus("Comment failed");
     }
   };
 
-
-
-
-
-
   return (
-
-    <div className= {style.commentSec}>
+    <div className={style.commentSec}>
       <h2>Comment Here!</h2>
       <form className="formInfo" onSubmit={handleSubmit}>
         <div>
-          <label >Name:</label>
-          <input className={style.user} type="text" id="user" name="user" value={formData.user} onChange={handleInputChange} placeholder="Your name" required/>
+          <label>Name:</label>
+          <input
+            className={style.user}
+            type="text"
+            id="user"
+            name="user"
+            value={formData.user}
+            onChange={handleInputChange}
+            placeholder="Your name"
+            required
+          />
         </div>
         <div>
-          <label >Comment:</label>
+          <label>Comment:</label>
           <textarea
-          className={style.comment}
+            className={style.comment}
             id="comment"
             name="content"
             value={formData.content}
@@ -95,14 +80,13 @@ export default function NewComment({ slug }: Props) {
             required
           />
         </div>
-
         <button type="submit">Submit Comment</button>
       </form>
-      {status && <p className={style.status}></p>}
-    
+      {status && <p className={style.status}>{status}</p>}
     </div>
   );
 }
+
 
 
 
